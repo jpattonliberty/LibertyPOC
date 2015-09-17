da2b62e7698aff2630b6254c2fa5386f8b94decf
jpatton <jonathan.c.patton@gmail.com>
2015-09-17 12:10:37 +0200

    Added the signalling code between client and callcenter screens

---
+++ Liberty.POC/Liberty.POC.UI/Scripts/callcenter-app.js
@@ -0,0 +1,13 @@
+﻿$(function () {
+    $(document).ready(function () {
+        $('#btnPong').hide();
+    });
+
+    var clientCallCenterHub = $.connection.clientCallCenterHub;
+
+    clientCallCenterHub.client.callCenterBroadcast = function (name, message) {
+        $('#divResponse').text(message + ' from ' + name);
+    };
+
+    $.connection.hub.start();
+});
