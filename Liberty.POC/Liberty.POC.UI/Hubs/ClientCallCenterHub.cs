b7abb00fd1dfa8ecd333628c4be051455ed28a31
jpatton <jonathan.c.patton@gmail.com>
2015-09-17 08:50:11 +0200

    Created the SignalR hub and OWIN Configuration

---
+++ Liberty.POC/Liberty.POC.UI/Hubs/ClientCallCenterHub.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNet.SignalR;
+
+namespace Liberty.POC.UI.Hubs
+{
+    public class ClientCallCenterHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+    }
+}
