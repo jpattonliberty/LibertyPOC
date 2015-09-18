2056ca32b468f958786fe0b482f55f0336c03044
jpatton <jonathan.c.patton@gmail.com>
2015-09-18 10:47:58 +0200

    Created the ClientDetailsModel

---
+++ Liberty.POC/Liberty.POC.UI/Models/ClientDetailsModel.cs
@@ -0,0 +1,13 @@
+﻿namespace Liberty.POC.UI.Models
+{
+    public class ClientDetailsModel
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string CurrentStep { get; set; }
+        public AddressModel AddressDetails { get; set; }
+        public ContactModel ContactDetail { get; set; }
+        public PersonalModel PersonalDetails { get; set; }
+        public bool IsCompleted{ get; set; }
+    }
+}
