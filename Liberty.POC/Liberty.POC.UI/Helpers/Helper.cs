f6c4b88dd0057f8a85de5c9cabee5b8afde181e4
jpatton <jonathan.c.patton@gmail.com>
2015-09-20 11:54:32 +0200

    completed the view binding for the call center process action

---
+++ Liberty.POC/Liberty.POC.UI/Helpers/Helper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Liberty.POC.UI.Helpers
+{
+    public static class UserInterfacehHelper
+    {
+        public static SelectList GetNumberOfDependants()
+        {
+            return new SelectList(Enumerable.Range(0, 11));
+        }
+
+        public static List<SelectListItem> GetProvinces()
+        {
+            return new List<SelectListItem>
+            {
+                new SelectListItem
+                {
+                    Text = "Gauteng"
+                }, 
+                 new SelectListItem
+                {
+                    Text = "Freestate"
+                }, 
+                  new SelectListItem
+                {
+                    Text = "Limpopo"
+                }, 
+                    new SelectListItem
+                {
+                    Text = "Mpumalanga"
+                }, 
+                     new SelectListItem
+                {
+                    Text = "North West"
+                }, 
+                     new SelectListItem
+                {
+                    Text = "KwaZulu-Natal"
+                }
+            };
+        }
+    }
+}
